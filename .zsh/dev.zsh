e6fc4112ed25f304df99a071b69cb8cf60723b23
beco-ippei <beco.ippei@gmail.com>
2013-03-25 14:10:09 +0900

    remove aws-ami and change aws-dev server name

--- .zsh/dev.zsh
+++ .zsh/dev.zsh
@@ -1,3 +1,2 @@
-alias aws='ssh -i ~/.ssh/beco-ubuntu.pem ippei@54.248.76.36'
-alias aws-ami='ssh -i ~/.ssh/beco-ubuntu.pem ippei@54.249.251.33'
+alias aws-dev='ssh -i ~/.ssh/beco-ubuntu.pem ippei@54.248.76.36'
 
